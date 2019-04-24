5dfa712a0af845ba4bd0a24c7ade2d9c1a642fd3
Carlos Silva <caarlos.silva06@gmail.com>
2019-04-24 06:50:40 -0300

    Add files via upload

--- event.php
+++ event.php
@@ -1,23 +1,28 @@
 <?php
-include_once("db_connect.php");
-$sqlEvents = "SELECT id, title, start_date, end_date FROM events LIMIT 20";
-$resultset = mysqli_query($conn, $sqlEvents) or die("database error:". mysqli_error($conn));
-$calendar = array();
-while( $rows = mysqli_fetch_assoc($resultset) ) {
-// convert date to milliseconds
-$start = strtotime($rows['start_date']) * 1000;
-$end = strtotime($rows['end_date']) * 1000;
-$calendar[] = array(
-'id' =>$rows['id'],
-'title' => $rows['title'],
-'url' => "#",
-"class" => 'event-important',
-'start' => "$start",
-'end' => "$end"
-);
-}
-$calendarData = array(
-"success" => 1,
-"result"=>$calendar);
-echo json_encode($calendarData);
+	include_once("DAO/conn.php");
+	$conexao = conectar();
+	$limite = 1;
+	$query = "SELECT * FROM Agenda WHERE ?";
+	$stmt = mysqli_prepare($conexao,$query);
+	mysqli_stmt_bind_param($stmt,"i",$limite);
+	$resultset = executar_SQL($conexao,$stmt);
+	$calendar = array();
+
+	while( $rows = mysqli_fetch_assoc($resultset) ) {
+	// convert date to milliseconds
+	    $start = strtotime($rows['start_date']) * 1000;
+	    $end = strtotime($rows['end_date']) * 1000;
+	    $calendar[] = array(
+	    'id' =>$rows['id'],
+	    'title' => $rows['title'],
+	    'url' => "#",
+	    "class" => 'event-important',
+	    'start' => "$start",
+	    'end' => "$end");
+	}
+	$calendarData = array(
+	"success" => 1,
+	"result"=>$calendar);
+	echo json_encode($calendarData);
+    desconectar($conexao);
 ?>
